4b66bdb08003b46fdc067289658e97d41a64b69c
Jorge Aguirre <jogonzal@microsoft.com>
2016-07-24 19:44:27 -0700

    Implement SumaFactorial

---
+++ SumaFactorial/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaFactorial
+{
+	public class Calculator
+	{
+		public static int CalculateFactorialSum(int numberAsInteger)
+		{
+			int accumulator = 0;
+			for (int i = 1; i < numberAsInteger + 1; i++)
+			{
+				accumulator += i;
+			}
+			return accumulator;
+		}
+	}
+}
--- SumaFactorial/Program.cs
+++ SumaFactorial/Program.cs
@@ -10,6 +10,18 @@ namespace SumaFactorial
 	{
 		static void Main(string[] args)
 		{
+			// Ask for number
+			Console.WriteLine("What is the number you want the factorial sum for?");
+			string numberAsText = Console.ReadLine();
+			int numberAsInteger = int.Parse(numberAsText);
+
+			// Calculate
+			int result = Calculator.CalculateFactorialSum(numberAsInteger);
+
+			// Print
+			Console.WriteLine("The result is " + result);
+
+			Console.ReadLine();
 		}
 	}
 }
